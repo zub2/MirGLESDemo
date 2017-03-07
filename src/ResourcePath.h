9f4396b84150668b7c0d7412e7e2e2f65a42e06b
David Kozub <zub.272@gmail.com>
2017-03-07 23:24:24 +0100

    add ResourcePath

---
+++ src/ResourcePath.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2017 David Kozub <zub at linux.fjfi.cvut.cz>
+ *
+ * This file is part of MirGLESDemo.
+ *
+ * MirGLESDemo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MirGLESDemo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MirGLESDemo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef RESOURCE_PATH_H
+#define RESOURCE_PATH_H
+
+#include <string>
+
+std::string getResourcePath(const std::string& resource);
+
+#endif // RESOURCE_PATH_H
