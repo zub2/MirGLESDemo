cbc953e72e52405a4b9888d06d3dc93121f24b60
David Kozub <zub.272@gmail.com>
2017-03-08 23:57:08 +0100

    add Program

---
+++ src/gl/Program.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 David Kozub <zub at linux.fjfi.cvut.cz>
+ *
+ * This file is part of MirGLESDemo.
+ *
+ * MirGLESDemo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MirGLESDemo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MirGLESDemo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "Program.h"
+
+#include <stdexcept>
+
+Program::Program(const Shader& vertexShader, const Shader& fragmentShader)
+{
+	m_program = glCreateProgram();
+	if (m_program == 0)
+		throw std::runtime_error("Can't create a new program.");
+
+	// no need to keep the shaders, OpenGL keeps them alive as long as the program lives
+	glAttachShader(m_program, vertexShader.getGLShader());
+	glAttachShader(m_program, fragmentShader.getGLShader());
+}
+
+void Program::link()
+{
+	glLinkProgram(m_program);
+
+	GLint isLinked = 0;
+	glGetProgramiv(m_program, GL_LINK_STATUS, &isLinked);
+	if (isLinked == 0)
+	{
+		GLint infoLogLen = 0;
+		glGetProgramiv(m_program, GL_INFO_LOG_LENGTH, &infoLogLen);
+		if (infoLogLen > 0)
+		{
+			std::unique_ptr<char[]> infoLog(new char[infoLogLen]);
+			glGetProgramInfoLog(m_program, infoLogLen, nullptr, infoLog.get());
+			throw std::runtime_error(std::string("Can't link program: ") + infoLog.get());
+		}
+		throw std::runtime_error("Can't link program: no error info available");
+	}
+}
+
+Program::~Program()
+{
+	glDeleteProgram(m_program);
+}
---
+++ src/gl/Program.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 David Kozub <zub at linux.fjfi.cvut.cz>
+ *
+ * This file is part of MirGLESDemo.
+ *
+ * MirGLESDemo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MirGLESDemo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MirGLESDemo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef GL_PROGRAM_H
+#define GL_PROGRAM_H
+
+#include "Shader.h"
+
+#include <GLES2/gl2.h>
+
+class Program
+{
+public:
+	Program(const Shader& vertexShader, const Shader& fragmentShader);
+	~Program();
+
+	void link();
+
+	GLuint getGLProgram() const
+	{
+		return m_program;
+	}
+
+	// allow move, disallow copy
+	Program& operator=(Program&&) = default;
+	Program(Program&&) = default;
+	Program& operator=(const Program&) = delete;
+	Program(const Program&) = delete;
+
+private:
+	GLuint m_program;
+};
+
+#endif // GL_PROGRAM_H
